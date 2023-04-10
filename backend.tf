8f490946012699e166b576ca1225c79b23ca854f
ahmet karayol <37154452+akarayol03@users.noreply.github.com>
2023-04-10 01:11:56 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,14 +4,14 @@ terraform {
     encrypt        = true
     key            = "terraform.tfstate"
     region         = "us-east-1"
-    dynamodb_table = "terraform-backend-us-ahmet-2"
-    read_capacity  = 20
-    write_capacity = 20
-    hash_key       = "LockID"
-    attribute {
-    name = "LockID"
-    type = "S"
-    }
+    #dynamodb_table = "terraform-backend-us-ahmet-2"
+    #read_capacity  = 20
+    #write_capacity = 20
+    #hash_key       = "LockID"
+    #attribute {
+    #name = "LockID"
+    #type = "S"
+    #}
   }
   required_version = ">= 0.14.0"
 }
