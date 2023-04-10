e4c68b37bab8888cc5497c09fa38fb3a34067f3d
ahmet karayol <37154452+akarayol03@users.noreply.github.com>
2023-04-09 23:38:27 -0500

    Create backend.tf
    Added backend

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-backend-us-ahmet-2"
+    encrypt        = true
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-backend-us-ahmet-2"
+  }
+}
